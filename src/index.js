7c6ceee974f7dbb56f83c5821e0c69a664020332
Michael Balagna <lunchmeat13@gmail.com>
2018-03-15 12:40:00 -0600

    install axios and making call to api!

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 
-const App = function() {
-  return <div> Hi!</div>;
+const App = () => {
+  axios.get('http://localhost:3000/api/v1/cans')
+    .then(function(response) {
+      console.log(response);
+    })
+
+  return <div>HI!</div>;
+
 }
 
-ReactDOM.render(App);
+ReactDOM.render(<App />, document.querySelector('.container'));
