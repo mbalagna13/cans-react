592c8a6a79285a3bee7cf05c709b6e295818d972
Michael Balagna <lunchmeat13@gmail.com>
2018-03-15 15:29:34 -0600

    Cans are listed and have better cards

--- src/components/cans_list_item.js
+++ src/components/cans_list_item.js
@@ -6,11 +6,11 @@ const CansListItem = (props) => {
 
   return (
     <li className="list-group-item">
-      <h1> {name} </h1>
-      <h3> {serial} </h3>
-      <h3> {size} </h3>
-      <h3>  {createdDate} </h3>
-      <h3> {modifiedDate} </h3>
+      <h1><u> Name of Can </u></h1> <h2>{name}</h2>
+      <h4><u> Serial Number </u></h4> <h4>{serial}</h4>
+      <h4><u> Size </u></h4> <h4>{size}</h4>
+      <h4><u> Created Date  </u></h4> <h4>{createdDate}</h4>
+      <h4><u> Modiefied Date </u></h4> <h4>{modifiedDate}</h4>
     </li>
   )
 }
